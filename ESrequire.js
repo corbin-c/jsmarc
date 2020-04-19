54c1b956539716b2b2c300edc4ed1931bb3a526a
Clément Corbin <corbin.clement@gmail.com>
2020-04-19 12:40:26 +0200

    mess cleaned

--- ESrequire.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-ESrequire = (path) => {
-  path = fs.readFileSync(path,"utf8");
-  path = path.replace("\nexport {","\nmodule.exports = {");
-  return eval(path);
-}
-module.exports = (path) => ESrequire(path);
